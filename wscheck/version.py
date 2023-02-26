d9fdaf5f198d6eb9c4408d27022ef2d44c079dd0
szantaii <szantaii@gmail.com>
2023-02-26 20:27:48 +0100

    wscheck/version: Replace legacy approach of specifying types using docstrings with type hinting

--- wscheck/version.py
+++ wscheck/version.py
@@ -1,29 +1,22 @@
+from typing import Tuple
+
 __version__ = (1, 3, 2)
 
 
 class Version:
-    def __init__(self):
+    def __init__(self) -> None:
         version_string_array = [str(v) for v in __version__]
         self.__version = '.'.join(version_string_array[:3])
         self.__release = '-'.join([self.__version] + version_string_array[3:])
 
     @property
-    def version_array(self):
-        """
-        :rtype: tuple
-        """
+    def version_array(self) -> Tuple[int, int, int]:
         return __version__
 
     @property
-    def version(self):
-        """
-        :rtype: str
-        """
+    def version(self) -> str:
         return self.__version
 
     @property
-    def release(self):
-        """
-        :rtype: str
-        """
+    def release(self) -> str:
         return self.__release
