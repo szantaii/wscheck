8b3e8f74d540626c1bff65f6843ec7fedb58de82
szantaii <szantaii@gmail.com>
2023-02-26 20:52:29 +0100

    setup.py: Update project classifiers
     - Remove Python2 language classifier
    
     - Add Python3 (and Python3 only) Language classifier
       (from Python 3.7 to 3.11)
    
       See the following table for the currently supported and to be released
       Python versions (see the Python version in the Branch column):
    
       | Branch | Schedule | Status   | First release | End of life |
       | ------ | -------- | -------- | ------------- | ----------- |
       |   main |  PEP 693 |  feature |    2023-10-02 |  2028-10    |
       |   3.11 |  PEP 664 |   bugfix |    2022-10-24 |  2027-10    |
       |    3.1 |  PEP 619 |   bugfix |    2021-10-04 |  2026-10    |
       |    3.9 |  PEP 596 | security |    2020-10-05 |  2025-10    |
       |    3.8 |  PEP 569 | security |    2019-10-14 |  2024-10    |
       |    3.7 |  PEP 537 | security |    2018-06-27 |  2023-06-27 |
    
       Status definitions:
        * feature: new features, bugfixes, and security fixes are accepted.
        * bugfix: bugfixes and security fixes are accepted, new binaries
                  are still released.
                  (Also called maintenance mode or stable release.)
        * security: only security fixes are accepted and no more binaries
                    are released, but new source-only versions can be
                    released.
    
       PEP links:
        * [PEP537](https://peps.python.org/pep-0537/)
        * [PEP569](https://peps.python.org/pep-0569/)
        * [PEP596](https://peps.python.org/pep-0596/)
        * [PEP619](https://peps.python.org/pep-0619/)
        * [PEP664](https://peps.python.org/pep-0664/)
        * [PEP693](https://peps.python.org/pep-0693/)
    
       The table above and the release information was taken
       from https://devguide.python.org/versions/.
    
     - Add Software Development Topic classifier

--- setup.py
+++ setup.py
@@ -41,10 +41,17 @@ setup(
     long_description=read('README.md'),
 
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development',
     ],
     keywords='whitespace ws check',
 
